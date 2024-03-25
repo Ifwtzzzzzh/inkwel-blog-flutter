3dc9e16c7f25a98c09eb63ea2b8d457ae2a1822b
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2024-03-25 13:42:58 +0700

    Signup Page UI

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:inkwel_blog_app/core/theme/app_palette.dart';
 
 class AppTheme {
-  static final darkThemeMode = ThemeData.dark();
+  static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
+
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPallete.gradient2),
+    ),
+  );
 }
--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:inkwel_blog_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:inkwel_blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -10,6 +12,27 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign Up.',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            AuthField(hintText: 'Name'),
+            const SizedBox(height: 15),
+            AuthField(hintText: 'Email'),
+            const SizedBox(height: 15),
+            AuthField(hintText: 'Password'),
+            const SizedBox(height: 20),
+            AuthGradientButton(),
+          ],
+        ),
+      ),
+    );
   }
 }
