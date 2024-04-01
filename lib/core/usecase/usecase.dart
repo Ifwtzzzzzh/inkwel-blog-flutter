df425aaec0541435756d9a48cf3dcf366f17e6df
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2024-04-01 11:47:37 +0700

    Auth Bloc for Sign Up

---
+++ lib/core/usecase/usecase.dart
@@ -0,0 +1,6 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:inkwel_blog_app/core/error/failures.dart';
+
+abstract interface class UseCase<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
