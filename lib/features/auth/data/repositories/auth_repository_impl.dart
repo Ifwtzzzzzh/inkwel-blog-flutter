21c2937656b056a4afe4222495b564549ddb8046
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2024-04-06 07:26:11 +0700

    Login Feature

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:fpdart/fpdart.dart';
 import 'package:inkwel_blog_app/core/error/exception.dart';
 import 'package:inkwel_blog_app/core/error/failures.dart';
 import 'package:inkwel_blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:inkwel_blog_app/features/auth/domain/entities/user.dart';
 import 'package:inkwel_blog_app/features/auth/domain/repository/auth_repository.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
@@ -9,25 +10,40 @@ class AuthRepositoryImpl implements AuthRepository {
   const AuthRepositoryImpl(this.remoteDataSource);
 
   @override
-  Future<Either<Failure, String>> loginWithEmailPassword(
-      {required String email, required String password}) {
-    // TODO: implement loginWithEmailPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, String>> signUpWithEmailPassword({
-    required String name,
+  Future<Either<Failure, User>> loginWithEmailPassword({
     required String email,
     required String password,
   }) async {
     try {
-      final userId = await remoteDataSource.signUpWithEmailPassword(
-        name: name,
+      final user = await remoteDataSource.loginWithEmailPassword(
         email: email,
         password: password,
       );
-      return right(userId);
+      return right(user);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, User>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    return _getUser(
+      () async => await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      ),
+    );
+  }
+
+  Future<Either<Failure, User>> _getUser(Future<User> Function() fn) async {
+    try {
+      final user = await fn();
+      return right(user);
     } on ServerException catch (e) {
       return left(Failure(e.message));
     }
--- lib/features/auth/domain/usecases/user_sign_up.dart
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -1,14 +1,15 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:inkwel_blog_app/core/error/failures.dart';
 import 'package:inkwel_blog_app/core/usecase/usecase.dart';
+import 'package:inkwel_blog_app/features/auth/domain/entities/user.dart';
 import 'package:inkwel_blog_app/features/auth/domain/repository/auth_repository.dart';
 
-class UserSignUp implements UseCase<String, UserSignUpParams> {
+class UserSignUp implements UseCase<User, UserSignUpParams> {
   final AuthRepository authRepository;
   const UserSignUp(this.authRepository);
 
   @override
-  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+  Future<Either<Failure, User>> call(UserSignUpParams params) async {
     return await authRepository.signUpWithEmailPassword(
       name: params.name,
       email: params.email,
