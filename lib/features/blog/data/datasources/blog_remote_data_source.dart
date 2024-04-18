48515618507a094529571d15a7fb6a63bfd7e2ec
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2024-04-18 09:26:36 +0700

    Uploading Blog

---
+++ lib/features/blog/data/datasources/blog_remote_data_source.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:inkwel_blog_app/core/error/exception.dart';
+import 'package:inkwel_blog_app/features/blog/data/models/blog_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class BlogRemoteDataSource {
+  Future<BlogModel> uploadBlog(BlogModel blog);
+  Future<String> uploadBlogImage({
+    required File image,
+    required BlogModel blog,
+  });
+}
+
+class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
+  final SupabaseClient supabaseClient;
+  BlogRemoteDataSourceImpl(this.supabaseClient);
+
+  @override
+  Future<BlogModel> uploadBlog(BlogModel blog) async {
+    try {
+      final blogData =
+          await supabaseClient.from('blogs').insert(blog.toJson()).select();
+      return BlogModel.fromJson(blogData.first);
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+
+  @override
+  Future<String> uploadBlogImage(
+      {required File image, required BlogModel blog}) {
+    // TODO: implement uploadBlogImage
+    throw UnimplementedError();
+  }
+}
